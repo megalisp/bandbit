ee4991c5e14574469cf04877b384e6e7bdee4059
( ) MEGA [ie: Jost Grant] <source.megalisp@proton.me>
2025-07-21 13:26:23 -0500

    Init commit.

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "bandbit")
+(define deps '("rackoon" "racket-cord"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("docs/index.scrbl" ())))
+(define pkg-desc "BANDBIT : Raccoon-Centered Discord Bot. Get Ban'd! ")
+(define version "0.0.1")
+(define pkg-authors '(megalisp))
